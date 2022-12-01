e9ad0d5937d3a5a46c635a237e31d2a67c2637e0
vh94 <valentinhaberl94@gmail.com>
2022-12-01 21:02:00 +0100

    APAAC prediction

--- scripts/calculate_tsne.R
+++ scripts/calculate_tsne.R
@@ -24,7 +24,7 @@ processing <- preProcess(APAAC, method = c("center", "scale", "pca","nzv")) # th
 transformed <- predict(processing, APAAC) 
 
 ggplot(transformed,aes(x=PC1,y=PC2,color=APAAC$EC))+geom_point()
-transformed_log<-as.data.frame(scale(log10(transformed)))
+transformed_log<-as.data.frame(scale(log10(transformed[-1])))
 ggplot(transformed_log,aes(x=PC1,y=PC2,color=APAAC$EC))+geom_point()
 
 
@@ -35,38 +35,22 @@ ggplot(APAAC,aes(log10(Pc1.A),log10(Pc2.Hydrophobicity.1) ,color=EC))+geom_point
 ### not very good and RTNSE performs scaling and PCA under the hood anyways ..
 ## and the default LR(200) is way to low
 learning_rate<-NROW(APAAC)/12
-transformed<-normalize_input(as.matrix(APAAC[-1]))
 
-Rtsne_fit<-Rtsne(transformed,
-                 pca=FALSE,
-                 pca_center = FALSE,
-                 pca_scale = FALSE,
-                 normalize = FALSE,
-                 is_distance=FALSE,
-                 max_iter=500,
-                 eta=learning_rate,
-                 theta=0.02,
-                 num_threads=0)
-
-plot1<-Rtsne_fit$Y %>% 
-  as.data.frame() %>%
-  mutate(EC=APAAC$EC) %>% 
-  ggplot(aes(x=V1,y=V2,color=EC))+
-  geom_point(alpha=0.5)+theme_classic()
-plot1
 
 # perplexity?? perp 3*30<NROW(APAAC)-1
-processing <- preProcess(APAAC, method = c("nzv","BoxCox","pca")) #PCA is perfor
+processing <- preProcess(APAAC[-1], method = c("nzv")) #PCA is perfor
 transformed <- predict(processing, APAAC) 
 
-transformend_norm<-normalize_input(as.matrix(transformed[-1]))
+dists<-dist(transformed,method = "manhattan")
+
+transformend_norm<-normalize_input(as.matrix(transformed))
 
 Rtsne_fit2<-Rtsne(transformend_norm,
                   pca=FALSE,
                   pca_center = FALSE,
                   pca_scale = FALSE,
                   normalize = FALSE,
-                  is_distance=FALSE,
+                  is_distance=TRUE,
                   max_iter=3500,
                   eta=learning_rate,
                   theta=0,
@@ -80,25 +64,5 @@ plot2<-Rtsne_fit2$Y %>%
   geom_point(alpha=0.5)+theme_classic()
 plot2
 
-##nnusing distances
 
 
-transformend_norm<-normalize_input(as.matrix(transformed[-1]))
-dists<-dist(transformend_norm,method = "manhattan")
-rm(APAAC)
-rm(transformed)
-rm(processing)
-rm(transformend_norm)
-gc()
-Rtsne_dist<-Rtsne(dists, is_distance=TRUE) 
-
-
-
-
-
-#NMDS
-
-
-fit <- cmdscale(dists,eig=TRUE, k=2)
-
-fit <- isoMDS(dists,k=2)
